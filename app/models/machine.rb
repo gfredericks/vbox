afa8984bdac88323d23a29c42bbd7fb2fa23e60c
Gary Fredericks <fredericksgary@gmail.com>
2010-06-27 09:25:39 -0400

    refresh method, start/stop, etc

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,6 +1,11 @@
 class Machine
+  attr_accessor :name, :info
   def initialize(name)
     @name=name
+    refresh
+  end
+
+  def refresh
     info = `VBoxManage showvminfo '#{name}'`
     re = /^\s*([^:]+?)\s*:\s*(.*?)\s*$/
     info = info.split("\n").select{|line|line=~re}
@@ -9,6 +14,19 @@ class Machine
       {$1 => $2}
     end
     @info = info.inject({}){|u,v|u.merge(v)}
+    @state = @info["State"]
+  end
+
+  def stop
+    :stub
+  end
+
+  def start
+    :stub
+  end
+
+  def running?
+    @state =~ /^running/
   end
 
   def Machine.all
